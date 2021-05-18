405fb6754ef4c10c31bbc595b556d09653443f65
Vyacheslav Kovalev <vyacheslav.kovalev@corp.mail.ru>
2021-05-18 20:23:49 +0300

    use api 29 emulator for ui tests

--- subprojects/android-test/ui-testing-core-app/build.gradle.kts
+++ subprojects/android-test/ui-testing-core-app/build.gradle.kts
@@ -96,11 +96,11 @@ instrumentation {
             tasks.named("check").dependsOn(tasks.named("instrumentationUi"))
         }
 
-        val emulator22 = CloudEmulator(
-            name = "api22",
-            api = 22,
-            model = "Android_SDK_built_for_x86",
-            image = emulatorImage(22, "740eb9a948"),
+        val emulator29 = CloudEmulator(
+            name = "api29",
+            api = 29,
+            model = "Android_SDK_built_for_x86_64",
+            image = emulatorImage(29, "915c1f20be"),
             cpuCoresRequest = "1",
             cpuCoresLimit = "1.3",
             memoryLimit = "4Gi"
@@ -110,8 +110,8 @@ instrumentation {
             reportSkippedTests = true
             filter = "ci"
 
-            targetsContainer.register("api22") {
-                deviceName = "API22"
+            targetsContainer.register("api29") {
+                deviceName = "API29"
 
                 scheduling {
                     quota {
@@ -120,10 +120,11 @@ instrumentation {
                     }
 
                     testsCountBasedReservation {
-                        device = emulator22
-                        maximum = 5
+                        device = emulator29
+                        maximum = 50
+                        maximum = 2
                         minimum = 2
-                        testsPerEmulator = 1
+                        testsPerEmulator = 3
                     }
                 }
             }
