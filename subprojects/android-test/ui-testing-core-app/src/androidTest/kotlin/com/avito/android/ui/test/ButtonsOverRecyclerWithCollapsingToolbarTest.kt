b6199b0df22f93a446c3daf9c05fa88beb76a7ff
Vyacheslav Kovalev <vyacheslav.kovalev@corp.mail.ru>
2021-05-18 15:07:20 +0300

    try to log in test

--- subprojects/android-test/ui-testing-core-app/src/androidTest/kotlin/com/avito/android/ui/test/ButtonsOverRecyclerWithCollapsingToolbarTest.kt
+++ subprojects/android-test/ui-testing-core-app/src/androidTest/kotlin/com/avito/android/ui/test/ButtonsOverRecyclerWithCollapsingToolbarTest.kt
@@ -17,7 +17,9 @@ class ButtonsOverRecyclerWithCollapsingToolbarTest {
     @Test
     fun listElement_elementClicked_whenThereIsOverlappedButtonInScreenWithCollapsingToolbar() {
         Screen.buttonsOverRecycler.list.cellAt(90).click()
-        Log.i("testy", "testy listElement_elementClicked_whenThereIsOverlappedButtonInScreenWithCollapsingToolbar")
+        Log.i("testy", "itesty listElement_elementClicked_whenThereIsOverlappedButtonInScreenWithCollapsingToolbar")
+        Log.d("testy", "dtesty listElement_elementClicked_whenThereIsOverlappedButtonInScreenWithCollapsingToolbar")
+        Log.e("testy", "etesty listElement_elementClicked_whenThereIsOverlappedButtonInScreenWithCollapsingToolbar")
         throw Exception("testys")
     }
 }
