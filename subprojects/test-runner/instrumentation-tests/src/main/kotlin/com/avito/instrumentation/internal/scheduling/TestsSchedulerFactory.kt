abf217bab8e06332448a45e48082f5fb585cc09b
Vyacheslav Kovalev <vyacheslav.kovalev@corp.mail.ru>
2021-05-18 12:26:54 +0300

    try to write to logcat txt file

--- subprojects/test-runner/instrumentation-tests/src/main/kotlin/com/avito/instrumentation/internal/scheduling/TestsSchedulerFactory.kt
+++ subprojects/test-runner/instrumentation-tests/src/main/kotlin/com/avito/instrumentation/internal/scheduling/TestsSchedulerFactory.kt
@@ -14,6 +14,7 @@ import com.avito.instrumentation.internal.report.listener.GsonReportParser
 import com.avito.instrumentation.internal.report.listener.LegacyTestArtifactsProcessor
 import com.avito.instrumentation.internal.report.listener.LogcatProcessor
 import com.avito.instrumentation.internal.report.listener.LogcatTestLifecycleListener
+import com.avito.instrumentation.internal.report.listener.LogcatTestToFileUploader
 import com.avito.instrumentation.internal.report.listener.ReportParser
 import com.avito.instrumentation.internal.report.listener.ReportProcessor
 import com.avito.instrumentation.internal.report.listener.ReportProcessorImpl
@@ -125,13 +126,17 @@ internal interface TestsSchedulerFactory {
 
             val retracer: ProguardRetracer = ProguardRetracer.Impl(params.proguardMappings)
 
-            val artifactsUploader: TestArtifactsUploader = AvitoFileStorageUploader(
-                RemoteStorageFactory.create(
-                    endpoint = params.fileStorageUrl.toHttpUrl(),
-                    httpClientProvider = httpClientProvider,
-                    isAndroidRuntime = false
-                )
+            val artifactsUploader: TestArtifactsUploader = LogcatTestToFileUploader(
+                outputDir = params.outputDir
             )
+            //
+            // val artifactsUploader: TestArtifactsUploader = AvitoFileStorageUploader(
+            //     RemoteStorageFactory.create(
+            //         endpoint = params.fileStorageUrl.toHttpUrl(),
+            //         httpClientProvider = httpClientProvider,
+            //         isAndroidRuntime = false
+            //     )
+            // )
 
             val logcatUploader = LogcatProcessor.Impl(
                 testArtifactsUploader = artifactsUploader,
