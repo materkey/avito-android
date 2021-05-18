71d333c6ab28f0484d0bb327fbef023af25ddffe
Vyacheslav Kovalev <vyacheslav.kovalev@corp.mail.ru>
2021-05-18 21:35:28 +0300

    Revert "add some runner logs"
    This reverts commit af70c4b4

--- subprojects/test-runner/test-inhouse-runner/src/main/kotlin/com/avito/android/runner/InHouseInstrumentationTestRunner.kt
+++ subprojects/test-runner/test-inhouse-runner/src/main/kotlin/com/avito/android/runner/InHouseInstrumentationTestRunner.kt
@@ -189,7 +189,6 @@ abstract class InHouseInstrumentationTestRunner :
     }
 
     override fun beforeOnCreate(arguments: Bundle) {
-        logger.debug("testy beforeOnCreate")
         injectTestMetadata(instrumentationArguments)
         logger.debug("Instrumentation arguments: $instrumentationArguments")
         val environment = testRunEnvironment.asRunEnvironmentOrThrow()
